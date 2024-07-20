8aa4ce07690f259c9f474378649898f1e1ac14dc
MZainMehar <mzainmehar-developer@outlook.com>
2024-07-20 09:00:13 +0500

    Add solutions for Chapter 4 practice set problems

---
+++ Chapter 4 - PS/Problem 3.py
@@ -0,0 +1,4 @@
+# Check that a tuple type cannot be changed in python.
+
+tuple = (1, 2, 3, 4, 5, 6)
+print(float(tuple)) # (1, 2, 3, 4, 5, 6)
---
+++ Chapter 4 - PS/Problem 4.py
@@ -0,0 +1,14 @@
+# Write a program to sum a list with 4 numbers.
+
+numbers = [1, 2, 3, 4]
+
+print(sum(numbers)) # 10
+
+#  Other ways to sum a list with 4 numbers:
+
+numbers = [1, 2, 3, 4]
+total = 0
+for number in numbers:
+    total += number
+
+print(total) # 10
