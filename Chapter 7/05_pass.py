ea6145c86622632bf947c75d8adb4e751623fe48
MZainMehar <mzainmehar-developer@outlook.com>
2024-07-26 19:10:09 +0500

    Add pass statement example in Chapter 7

---
+++ Chapter 7/05_pass.py
@@ -0,0 +1,11 @@
+# Pass statement in a loop is used to do nothing when a certain condition is met.
+
+# Example:
+
+for i in range(100):
+    pass # Do nothing
+
+i = 0
+while (i<10):
+    print(i)
+    i += 1
