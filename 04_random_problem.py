75565bb03ff83e211019643bdc8e39f62faa74d9
MZainMehar <mzainmehar-developer@outlook.com>
2024-08-07 17:53:15 +0500

    name_shortener function

---
+++ 04_random_problem.py
@@ -0,0 +1,9 @@
+# Name shortener function for a given name and maximum length
+
+def name_shortener(name, max_Length):
+    if(len(name) > max_Length):
+        return name[:max_Length]
+    else:
+        return name
+    
+print(name_shortener("Siddharth", 5))
