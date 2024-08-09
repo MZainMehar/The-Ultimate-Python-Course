262eb99ee3b9cf855938b0bacb2bd67b02984224
MZainMehar <mzainmehar-developer@outlook.com>
2024-08-09 17:39:25 +0500

    Refactor random number generation function for improved readability and efficiency

--- 05_random_task.py
+++ 05_random_task.py
@@ -18,6 +18,21 @@ print(random_number(10))
 print_random_number(100)
 
 
+# Function to print decimal/floating point number
+
+def print_random_float():
+    print(random.uniform(0, 100))
+
+    # round off the number to 2 decimal places
+    print(round(random.uniform(0, 100), 1))
+
+
+print_random_float()
+
+
+
+
+
 
 
 
